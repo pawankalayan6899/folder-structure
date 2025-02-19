f38f16d6b1a4f5c133e3785eb60453b994eef72e
pawankalayan6899 <99861196+pawankalayan6899@users.noreply.github.com>
2025-02-19 15:18:00 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// App.js (index.js)
+
 import React from 'react';
 import FolderStructure from './components/FolderStructure.js';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS (optional but good practice)
